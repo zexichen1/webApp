31f2f2fe10fdd9430315e669209f473d4aa13579
RickyCUHK <119010110@link.cuhk.edu.cn>
2024-11-15 18:46:47 -0800

    LAB

---
+++ src/Labs/Lab5/index.tsx
@@ -0,0 +1,28 @@
+import PathParameters from "./PathParameters";
+import EnvironmentVariables from "./EnvironmentVariables";
+import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
+import HttpClient from "./HttpClient";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+export default function Lab5() {
+    return (
+      <div id="wd-lab5">
+        <h2>Lab 5</h2>
+        <div className="list-group">
+          <a href={`${REMOTE_SERVER}/lab5/welcome`} className="list-group-item">
+            Welcome
+          </a>
+        </div><hr/>
+        <EnvironmentVariables />
+        <PathParameters />
+        <WorkingWithObjects />
+        <WorkingWithArrays />
+        <HttpClient />
+        <WorkingWithObjectsAsynchronously />
+        <WorkingWithArraysAsynchronously />
+      </div>
+    );
+  }
+  
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -27,9 +27,15 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-a3" href="#/Labs/Lab4"
+        <a id="wd-a4" href="#/Labs/Lab4"
           className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}>
           Lab 4
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-a5" href="#/Labs/Lab5"
+          className={`nav-link ${pathname.includes("Lab5") ? "active" : ""}`}>
+          Lab 5
         </a>
       </li>
       <li className="nav-item">
